81ae4d2b2a3ac01f70f05493812b5de7d4bb754b
HamInGyu <hfire1207@gmail.com>
2021-10-14 15:45:18 +0900

    feat: useNotification 완성

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import useFullscreen from "./useFullscreen";
+import useNotification from "./useNotification";
 
 function App() {
-  const onFullScreen = (isFull) => {
-    console.log(isFull ? "We are full" : "We are small");
-  };
-  const { ref, triggerFull, exitFull } = useFullscreen(onFullScreen);
+  const triggerNotification = useNotification("Hello!", {
+    body: "Hello friends!",
+  });
   return (
     <div className="App">
-      <div ref={ref} style={{ backgroundColor: "#121212" }}>
-        <img
-          src="https://images.unsplash.com/photo-1593642702909-dec73df255d7?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1469&q=80"
-          alt="fullScreenTest"
-          style={{ width: "400px" }}
-        />
-        <button onClick={exitFull}>Exit fullscreen</button>
-      </div>
-      <button onClick={triggerFull}>Make fullscreen</button>
+      <button onClick={triggerNotification}>Hello</button>
     </div>
   );
 }
